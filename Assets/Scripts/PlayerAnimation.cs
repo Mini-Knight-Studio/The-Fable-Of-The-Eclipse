8b65ca99167ec8df079b123c39f240156ca20849
M4xp0w3rg4m3s <156011390+M4xp0w3rg4m3s@users.noreply.github.com>
2026-03-01 02:42:04 +0100

    Visual feedback model change
    - Now the player's model changes color depending on current state (idle, walking, dashing)

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -15,10 +15,18 @@ class PlayerAnimation : Component
     public string walkClipName = "walk";
     public string dashClipName = "dash";
 
+    private Entity idleMesh;
+    private Entity walkMesh;
+    private Entity dashMesh;
+
     public void OnCreate()
     {
         playerMovement = entity.GetComponent<PlayerMovement>();
         animator = entity.GetComponent<Animator>();
+
+        idleMesh = Entity.FindEntityByName("brightwhiskers_idle");
+        walkMesh = Entity.FindEntityByName("brightwhiskers_walk");
+        dashMesh = Entity.FindEntityByName("brightwhiskers_dash");
     }
 
     public void OnUpdate()
@@ -51,6 +59,9 @@ class PlayerAnimation : Component
         Console.WriteLine("I'm idle");
         toIdle = false;
         toWalk = true;
+        idleMesh.SetActive(true);
+        walkMesh.SetActive(false);
+        dashMesh.SetActive(false);
     }
 
     private void Move()
@@ -59,6 +70,9 @@ class PlayerAnimation : Component
         Console.WriteLine("I'm moving");
         toWalk = false;
         toIdle = true;
+        idleMesh.SetActive(false);
+        walkMesh.SetActive(true);
+        dashMesh.SetActive(false);
     }
 
     private void Dash()
@@ -68,5 +82,8 @@ class PlayerAnimation : Component
         toDash = false;
         toWalk = true;
         toIdle = true;
+        idleMesh.SetActive(false);
+        walkMesh.SetActive(false);
+        dashMesh.SetActive(true);
     }
 };
