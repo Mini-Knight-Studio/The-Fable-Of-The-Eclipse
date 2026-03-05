108e7e20eff3e4651429c60f18a8cedb874171ee
M4xp0w3rg4m3s <156011390+M4xp0w3rg4m3s@users.noreply.github.com>
2026-03-05 21:07:40 +0100

    Moving Pillars 3

--- Assets/Scripts/MovingPillar.cs
+++ Assets/Scripts/MovingPillar.cs
@@ -3,83 +3,99 @@ using Loopie;
 
 class MovingPillar : Component
 {
-    private BoxCollider upCollider;
-    private BoxCollider downCollider;
-    private BoxCollider rightCollider;
-    private BoxCollider leftCollider;
+    private BoxCollider zPlusCollider;
+    private BoxCollider zMinusCollider;
+    private BoxCollider xPlusCollider;
+    private BoxCollider xMinusCollider;
 
-    public string upColliderName;
-    public string downColliderName;
-    public string rightColliderName;
-    public string leftColliderName;
+    public string zPlusColliderName;
+    public string zMinusColliderName;
+    public string xPlusColliderName;
+    public string xMinusColliderName;
 
     public float collisionCooldown = 2.0f;
     private float collisionTimer = 0.0f;
 
     public float movementSpeed = 2.0f;
+    public float movementDistance = 3.0f;
+
+    private Vector3 startPosition;
+    private float moveTimer = 0.0f;
+    private float moveDuration = 0.0f;
+
+    private bool isMoving = false;
+    private Vector3 targetPosition;
 
     void OnCreate()
     {
-        upCollider = Entity.FindEntityByName(upColliderName).GetComponent<BoxCollider>();
-        downCollider = Entity.FindEntityByName(downColliderName).GetComponent<BoxCollider>();
-        rightCollider = Entity.FindEntityByName(rightColliderName).GetComponent<BoxCollider>();
-        leftCollider = Entity.FindEntityByName(leftColliderName).GetComponent<BoxCollider>();
+        zPlusCollider = Entity.FindEntityByName(zPlusColliderName).GetComponent<BoxCollider>();
+        zMinusCollider = Entity.FindEntityByName(zMinusColliderName).GetComponent<BoxCollider>();
+        xPlusCollider = Entity.FindEntityByName(xPlusColliderName).GetComponent<BoxCollider>();
+        xMinusCollider = Entity.FindEntityByName(xMinusColliderName).GetComponent<BoxCollider>();
     }
 
     void OnUpdate()
     {
-        HandleCollision();
+        if (isMoving)
+        {
+            MoveTowardsTarget();
+        }
+        else
+        {
+            HandleCollision();
+        }
     }
 
     void HandleCollision()
     {
         if (collisionTimer < collisionCooldown)
         {
-            collisionTimer++;
+            collisionTimer += Time.deltaTime;
             return;
         }
 
-        if (upCollider.HasCollided)
-        {
-            HandleUpCollision();
-        }
-        else if (downCollider.HasCollided)
-        {
-            HandleDownCollision();
-        }
-        else if (rightCollider.HasCollided)
-        {
-            HandleRightCollision();
-        }
-        else if (leftCollider.HasCollided)
-        {
-            HandleLeftCollision();
-        }
+        Vector3 pos = entity.transform.position;
 
-        collisionTimer++;
+        if (zPlusCollider.HasCollided)
+            StartMovement(pos + new Vector3(0, 0, movementDistance));
+        else if (zMinusCollider.HasCollided)
+            StartMovement(pos + new Vector3(0, 0, -movementDistance));
+        else if (xPlusCollider.HasCollided)
+            StartMovement(pos + new Vector3(movementDistance, 0, 0));
+        else if (xMinusCollider.HasCollided)
+            StartMovement(pos + new Vector3(-movementDistance, 0, 0));
+
+        collisionTimer += Time.deltaTime;
     }
 
-    void HandleUpCollision()
+    void StartMovement(Vector3 newTarget)
     {
+        startPosition = entity.transform.position;
+        targetPosition = newTarget;
 
+        Vector3 difference = targetPosition - startPosition;
+        float distance = (float)difference.magnitude;
+
+        moveDuration = distance / movementSpeed;
+        moveTimer = 0.0f;
+
+        isMoving = true;
         collisionTimer = 0;
     }
 
-    void HandleDownCollision()
+    void MoveTowardsTarget()
     {
+        moveTimer += Time.deltaTime;
 
-        collisionTimer = 0;
-    }
+        float t = moveTimer / moveDuration;
 
-    void HandleRightCollision()
-    {
+        if (t >= 1.0f)
+        {
+            entity.transform.position = targetPosition;
+            isMoving = false;
+            return;
+        }
 
-        collisionTimer = 0;
-    }
-
-    void HandleLeftCollision()
-    {
-
-        collisionTimer = 0;
+        entity.transform.position = Vector3.Lerp(startPosition, targetPosition, t);
     }
 };
