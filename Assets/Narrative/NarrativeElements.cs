404ab52cca27874800293fcd35443ac02f084ee1
Wakiren <javipedre4@gmail.com>
2026-03-16 22:36:42 +0100

    Simple Text System Working

---
+++ Assets/Narrative/NarrativeElements.cs
@@ -0,0 +1,31 @@
+using System;
+using Loopie;
+
+class NarrativeElements : Component
+{
+    public string entityName = "TextNarrative";
+    public Text text;
+    public Entity textEntity;
+
+    public string textValue = "";
+    private BoxCollider boxCollider;
+
+    void OnCreate()
+    {
+        textEntity = Entity.FindEntityByName(entityName);
+        text = textEntity.GetComponent<Text>();
+        boxCollider = entity.GetComponent<BoxCollider>();
+    }
+
+    void OnUpdate()
+    {
+        if (boxCollider.HasCollided) 
+        {
+            text.Value = textValue;
+        }
+        if (boxCollider.HasEndedCollision)
+        {
+            text.Value = "";
+        }
+    }
+};
