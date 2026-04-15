895a3db14f365bc19d2d0d4e7d6a51fd24102009
Ana Alcázar Cobo <ana.alcazar@estudiantat.upc.edu>
2026-04-15 10:31:58 +0200

    Update

--- Assets/Scripts/Enemies/EnemyList.cs
+++ Assets/Scripts/Enemies/EnemyList.cs
@@ -1,8 +1,15 @@
-using System;
 using Loopie;
+using System;
+using System.Text;
 
 class EnemyList : Component
 {
+    class EnemyDirection
+    {
+        public string EnemyName;
+        public Entity ReferenceEntity;
+    }
+
     void OnCreate()
     {
 
@@ -11,5 +18,33 @@ class EnemyList : Component
     void OnUpdate()
     {
 
+    }
+
+    string FormatName(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return input;
+
+        const string suffix = "_Reference";
+
+        if (input.EndsWith(suffix))
+            input = input.Substring(0, input.Length - suffix.Length);
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            char c = input[i];
+
+            if (i > 0
+                && char.IsUpper(c)
+                && !char.IsUpper(input[i - 1]))
+            {
+                result.Append(' ');
+            }
+
+            result.Append(c);
+        }
+        return result.ToString();
     }
 };
--- Assets/Scripts/Enemies/Spawner.cs
+++ Assets/Scripts/Enemies/Spawner.cs
@@ -3,13 +3,41 @@ using Loopie;
 
 class Spawner : Component
 {
+    public Entity entityToClone;
+    public float distance;
+    private Entity player;
+    public bool alreadySpawned {  get; private set; }
+
     void OnCreate()
     {
-
+        player = Entity.FindEntityByName("Player");
     }
 
     void OnUpdate()
     {
+        if(!alreadySpawned && Vector3.Distance(transform.position, player.transform.position) <= distance)
+        {
+            alreadySpawned = true;
+            Debug.Log("1");
+            Entity newClone = entityToClone.Clone(true);//Hello ^^
+            Debug.Log("2");
+            newClone.Name.Replace("Reference", "");
+            Debug.Log("3");
+            newClone.transform.position = transform.position;
+            newClone.SetActive(true);
+        }
+    }
 
+    public void ModifySpawnerStatus(bool state)
+    {
+        alreadySpawned = state;
+    }
+
+    void OnDrawGizmo()
+    {
+        if (!alreadySpawned)
+        {
+            Gizmo.DrawLine(transform.position + transform.Up, transform.position + transform.Up + (player.transform.position - transform.position).normalized*distance, Color.White);
+        }
     }
 };
