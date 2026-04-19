9314f9b52a5a36965c278a8f116dc2d68e2f25f2
Víctor Cárdenas <victor.cardenas@estudiantat.upc.edu>
2026-04-19 11:01:23 +0200

    Update

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,13 +1,11 @@
 using System;
 using System.Collections.Generic;
 
-
 public static class PlayerStats
 {
     public static int savedHealth = 5;
-    public static int savedOrbs = 0;
 
     public static bool isInitialized = false;
 
     public static List<string> collectedItems = new List<string>();
-}; 
+};
--- Assets/Scripts/Player/PlayerStatsSync.cs
+++ Assets/Scripts/Player/PlayerStatsSync.cs
@@ -4,49 +4,33 @@ using Loopie;
 public class PlayerStatsSync : Component
 {
     private Health playerHealth;
-    private OrbInventory orbInventory;
 
     void OnCreate()
     {
         playerHealth = entity.GetComponent<Health>();
-        orbInventory = entity.GetComponent<OrbInventory>();
 
-      
         if (!PlayerStats.isInitialized)
         {
             if (playerHealth != null)
             {
                 PlayerStats.savedHealth = playerHealth.GetMaxHealth();
             }
-            PlayerStats.savedOrbs = 0;
-            PlayerStats.isInitialized = true; 
+            PlayerStats.isInitialized = true;
         }
         else
         {
-          
             if (playerHealth != null)
             {
                 playerHealth.actualHealth = PlayerStats.savedHealth;
-            }
-
-            if (orbInventory != null)
-            {
-                orbInventory.orbsCollected = PlayerStats.savedOrbs;
             }
         }
     }
 
     void OnUpdate()
     {
-       
         if (playerHealth != null)
         {
             PlayerStats.savedHealth = playerHealth.GetActualHealth();
         }
-
-        if (orbInventory != null)
-        {
-            PlayerStats.savedOrbs = orbInventory.orbsCollected;
-        }
     }
-}; 
+};
