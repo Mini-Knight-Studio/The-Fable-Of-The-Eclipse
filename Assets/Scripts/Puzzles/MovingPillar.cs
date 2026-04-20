33dc7666d7a746bbf8bb5de21d79a87abe0b1923
M4xp0w3rg4m3s <156011390+M4xp0w3rg4m3s@users.noreply.github.com>
2026-04-20 19:37:54 +0200

    Pillar fix puzzle 2-2

--- Assets/Scripts/Puzzles/MovingPillar.cs
+++ Assets/Scripts/Puzzles/MovingPillar.cs
@@ -165,7 +165,7 @@ class MovingPillar : Component
         origin += direction * 0.05f;
 
         RaycastHit hit;
-        bool blocked = Collisions.Raycast(origin, direction, tileSize, out hit);
+        bool blocked = Collisions.Raycast(origin, direction, tileSize, out hit, Collisions.GetLayerBit("PillarLimits")| Collisions.GetLayerBit("WorldLimits"));
 
         pushForward.SetActive(true);
         pushBack.SetActive(true);
