c75a1802c588878bfc17a36d186ae6862f5289c4
XeivUPC <adria.garcia.almaden@estudiantat.upc.edu>
2026-04-27 17:49:12 +0200

    More feedback

---
+++ Assets/Scripts/Interactuables/RollingBridge.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections;
+using Loopie;
+
+class RollingBridge : Component
+{
+    private BoxCollider collider;
+
+    [Header("References")]
+    public Entity bridgeBase;
+    public Entity blockingCollider;
+
+    [Header("Feedback")]
+    public Entity audioSourceEntity;
+    private AudioSource audioSource;
+
+    public Entity particlesEntity;
+    private ParticleComponent particles;
+
+    [Header("Positions")]
+    public Loopie.Vector3 finalPos;
+    public Loopie.Vector3 finalRotation = Loopie.Vector3.Zero;
+
+    public Loopie.Vector3 standingPos;
+    public Loopie.Vector3 standingRotation;
+
+    [Header("Settings")]
+    public float duration = 2.0f;
+    public float easeIntensity = 1;
+    private float elapsedTime = 0f;
+
+    private bool animationFinished = false;
+    private bool animationStarted = false;
+
+    void OnCreate()
+    {
+        collider = entity.GetComponent<BoxCollider>();
+        audioSource = audioSourceEntity.GetComponent<AudioSource>();
+        particles = particlesEntity.GetComponent<ParticleComponent>();
+    }
+
+    void OnUpdate()
+    {
+        if (animationFinished) return;
+
+        if (DatabaseRegistry.puzzlesDB.Puzzles.BridgePushedDown == true)
+        {
+            bridgeBase.transform.local_position = finalPos;
+            bridgeBase.transform.local_rotation = finalRotation;
+            blockingCollider.GetComponent<BoxCollider>().SetActive(false);
+            animationFinished = true;
+        }
+
+        if (!animationFinished && !animationStarted && collider.IsColliding && Player.Instance.Movement.IsDashing())
+        {
+            animationStarted = true;
+
+            if (audioSource != null)
+                audioSource.Play();
+
+            StartCoroutine(HandleMovement());
+        }
+    }
+
+    IEnumerator HandleMovement()
+    {
+        elapsedTime = 0;
+        while (true)
+        {
+            elapsedTime += Time.deltaTime;
+
+            float t = elapsedTime / duration;
+            float curvedT = Mathf.Pow(t, easeIntensity);
+
+            Loopie.Vector3 pos = Loopie.Vector3.Lerp(standingPos, finalPos, curvedT);
+            Loopie.Vector3 rot = Loopie.Vector3.Lerp(standingRotation, finalRotation, curvedT);
+
+            bridgeBase.transform.local_position = pos;
+            bridgeBase.transform.local_rotation = rot;
+
+            if (t >= 1f)
+            {
+                break;
+            }
+            yield return null;
+        }
+
+        animationFinished = true;
+        blockingCollider.GetComponent<BoxCollider>().SetActive(false);
+        DatabaseRegistry.puzzlesDB.Puzzles.BridgePushedDown = true;
+
+        if (particles != null)
+            particles.Play();
+        yield return new WaitForSeconds(0.3f);
+        if (particles != null)
+            particles.Stop();
+    }
+}
