2a170b7dd02640cbe50fdbfa1a91af9b960c84cf
Max <max.mateo@estudiantat.upc.edu>
2026-04-30 14:43:33 +0200

    Puzzle door

--- Assets/Scripts/Interactuables/PuzzleDoor.cs
+++ Assets/Scripts/Interactuables/PuzzleDoor.cs
@@ -7,70 +7,118 @@ class PuzzleDoor : Component
     [Header("References")]
     public Entity rightDoor;
     public Entity leftDoor;
-    public Entity ownerChest;
-    public Entity focusPointOnCollect;
+    public Entity staticKey;
+    public Entity animatedKey;
+    public Entity focusPointOnInsert;
 
     [Header("Settings")]
-    public float amplitude = 0.5f;
-    public float speed = 2.0f;
-    public bool started = false;
-    public float collectTime = 1;
+    public Vector3 finalRightDoorRot = Vector3.Zero;
+    public Vector3 finalLefttDoorRot = Vector3.Zero;
+    public float keyTravelDuration = 1.0f;
+    public float doorOpenDuration = 2.0f;
+    public float pauseBeforeOpening = 0.5f;
+    public float easeIntensity = 1.5f;
 
-    private Vector3 startLocalPos;
-    private float time;
-    private BoxCollider collider;
-    private ParticleComponent particles;
-    private bool collected = false;
+    private bool isOpening = false;
+    private bool hasOpened = false;
+
+    private Vector3 initialRightDoorPos;
+    private Vector3 initialRightDoorRot;
+    private Vector3 initialLeftDoorPos;
+    private Vector3 initialLeftDoorRot;
+    private Vector3 finalKeyScale;
 
     void OnCreate()
     {
+        finalKeyScale = animatedKey.transform.scale;
 
+        staticKey.SetActive(false);
+        animatedKey.SetActive(false);
+
+        initialRightDoorPos = rightDoor.transform.local_position;
+        initialRightDoorRot = rightDoor.transform.local_rotation;
+        initialLeftDoorPos = leftDoor.transform.local_position;
+        initialLeftDoorRot = leftDoor.transform.local_rotation;
     }
 
     void OnUpdate()
     {
+        if (hasOpened || isOpening) return;
 
+        if (entity.GetComponent<BoxCollider>().IsColliding && Input.IsKeyDown(KeyCode.E) /*&& player has key*/)
+        {
+            isOpening = true;
+            StartCoroutine(OpenDoors());
+        }
     }
 
     IEnumerator OpenDoors()
     {
-        collected = true;
-        Entity player = Player.Instance.entity;
-        Vector3 initialPosition = transform.position;
-        Vector3 initialScale = transform.scale;
-        float timer = 0;
+        animatedKey.SetActive(true);
+        animatedKey.transform.position = Player.Instance.transform.position + new Vector3(0, 2, 0);
+        animatedKey.transform.scale = Vector3.Zero;
+
+        Vector3 startKeyPos = animatedKey.transform.position;
+        Vector3 targetKeyPos = staticKey.transform.position;
+
+        Vector3 startKeyScale = Vector3.Zero;
+        Vector3 targetKeyScale = finalKeyScale;
+
+        float elapsedTime = 0f;
+
+        Player.Instance.Camera.FocusOnPoint(focusPointOnInsert.transform.position, 15, 7);
+
+        yield return new WaitForSeconds(1);
+
         while (true)
         {
-            timer += Time.deltaTime;
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / keyTravelDuration;
 
-            transform.position = Vector3.Lerp(initialPosition, player.transform.position + new Vector3(0, 2, 0), Mathf.Clamp01(timer / collectTime));
-            transform.scale = Vector3.Lerp(initialScale, Vector3.Zero, Mathf.Clamp01(timer / collectTime));
+            float curvedT = Mathf.Pow(t, easeIntensity);
 
-            if (timer >= collectTime)
+            animatedKey.transform.position = Vector3.Lerp(startKeyPos, targetKeyPos, curvedT);
+            animatedKey.transform.scale = Vector3.Lerp(startKeyScale, targetKeyScale, curvedT);
+
+            if (t >= 1f)
+            {
+                animatedKey.transform.position = targetKeyPos;
+                animatedKey.transform.scale = targetKeyScale;
                 break;
-            else
-                yield return null;
+            }
+            yield return null;
         }
 
+        animatedKey.SetActive(false);
+        staticKey.SetActive(true);
 
-        if (entity.HasComponent<AudioSource>())
+        yield return new WaitForSeconds(pauseBeforeOpening);
+
+        elapsedTime = 0f;
+
+        while (true)
         {
-            entity.GetComponent<AudioSource>().Stop();
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / doorOpenDuration;
+
+            float curvedT = Mathf.Pow(t, easeIntensity);
+
+            rightDoor.transform.local_rotation = Vector3.Lerp(initialRightDoorRot, finalRightDoorRot, curvedT);
+
+            leftDoor.transform.local_rotation = Vector3.Lerp(initialLeftDoorRot, finalLefttDoorRot, curvedT);
+
+            if (t >= 1f)
+            {
+                rightDoor.transform.local_rotation = finalRightDoorRot;
+                leftDoor.transform.local_rotation = finalLefttDoorRot;
+                break;
+            }
+            yield return null;
         }
 
-        if (ownerChest != null) ownerChest.GetComponent<Chest>().RewardCollected();
+        Player.Instance.Camera.StopFocus();
 
-        entity.SetActive(false);
-
-        if (focusPointOnCollect != null)
-        {
-            Player.Instance.Camera.FocusOnPoint(focusPointOnCollect.transform.position, 15, 7);
-
-            yield return new WaitForSeconds(4);
-
-            Player.Instance.Camera.StopFocus();
-        }
-
+        hasOpened = true;
 
         yield return null;
     }
@@ -79,4 +127,4 @@ class PuzzleDoor : Component
     {
         StopAllOwnedCoroutines();
     }
-};
+}
