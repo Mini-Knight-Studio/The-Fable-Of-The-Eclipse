5112e40f5245ae21ed152326b0a26be2226714c4
XeivUPC <adria.garcia.almaden@estudiantat.upc.edu>
2026-05-02 16:24:47 +0200

    New HealthBar + new Acorn

--- Assets/Scripts/UI/HealtBar/HealthSlot.cs
+++ Assets/Scripts/UI/HealtBar/HealthSlot.cs
@@ -6,6 +6,12 @@ class HealthSlot : Component
     public Entity fullSlotEntity;
     public Entity halfSlotEntity;
 
+    public Image fullSlotImage;
+
+    void OnCreate()
+    {
+        fullSlotImage = fullSlotEntity.GetComponent<Image>();
+    }
 
     public void UpdateVisuals(int value)
     {
@@ -13,5 +19,15 @@ class HealthSlot : Component
         halfSlotEntity.SetActive(value==1);
     }
 
+    public void Lock()
+    {
+        halfSlotEntity.SetActive(false);
+        fullSlotEntity.SetActive(true);
+        fullSlotImage.SetTint(new Vector4(0.45f, 0, 1, 1));
+    }
 
+    public void Unlock()
+    {
+        fullSlotImage.SetTint(new Vector4(1,1,1,1));
+    }
 }
