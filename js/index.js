ba24f4134071ce71bda69a6ad8aef29dffb44e55
AnaAlcazarCobo <ana.alcazar@estudiantat.upc.edu>
2026-05-20 19:21:46 +0200

    Update

--- js/index.js
+++ js/index.js
@@ -3,3 +3,21 @@ function SetSource(route)
     var iframe = document.getElementById('frame');
     iframe.setAttribute('src', 'html/' + route + '.html');
 }
+
+function ChangeMenuVisibility()
+{
+    var button = document.getElementById('menu-hider');
+    var actualstatus = button.getAttribute('status');
+    var menu = document.getElementById('menu');
+
+    if(actualstatus === 'hiden')
+    {
+        button.setAttribute('status', 'shown');
+        menu.style.display = 'flex';
+    }
+    if(actualstatus === 'shown')
+    {
+        button.setAttribute('status', 'hiden');
+        menu.style.display = 'none';
+    }
+}
