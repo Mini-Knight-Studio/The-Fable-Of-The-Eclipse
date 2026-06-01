7ed3507bdcd5eff704c33cee91ddd69f6a043427
XeivUPC <adria.garcia.almaden@estudiantat.upc.edu>
2026-06-01 21:00:13 +0200

    Boss New Version Start

---
+++ Assets/Scripts/Audio/AudioEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Loopie;
+
+public class AudioEntity : Component
+{
+    [ShowInInspector] private string Key;
+    private AudioSource source;
+
+    void OnCreate()
+    {
+        source = entity.GetComponent<AudioSource>();
+    }
+    void OnPostCreate()
+    {
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.Register(this);
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.Unregister(this);
+        }
+    }
+
+    public string GetKey() { return Key; }
+};
---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Loopie;
+
+public class AudioManager : Component
+{
+    public static AudioManager Instance { get; private set; }
+
+
+    private Dictionary<string, AudioEntity> audios = new Dictionary<string, AudioEntity>();
+
+    void OnCreate()
+    {
+        if (Instance != null)
+        {
+            entity.Destroy();
+            return;
+        }
+        DontDestroyOnLoad();
+
+    }
+
+    public void Register(AudioEntity audioEntity)
+    {
+        string key = audioEntity.GetKey();
+        if (audios.ContainsKey(key))
+            return;
+        audios.Add(key, audioEntity);
+    }
+
+    public void Unregister(AudioEntity audioEntity)
+    {
+        string key = audioEntity.GetKey();
+        if (!audios.ContainsKey(key))
+            return;
+        audios.Remove(key);
+    }
+};
