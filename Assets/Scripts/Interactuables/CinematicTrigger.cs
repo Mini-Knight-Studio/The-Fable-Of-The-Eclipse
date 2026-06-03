6492ed1d09364f3546e056cacc1131f10cc3da0b
XeivUPC <adria.garcia.almaden@estudiantat.upc.edu>
2026-06-03 19:04:32 +0200

    Cinematics Eatly Implementation

---
+++ Assets/Scripts/Interactuables/CinematicTrigger.cs
@@ -0,0 +1,65 @@
+﻿using Loopie;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+class CinematicTrigger : Component
+{
+    [Header("Settings")]
+    public bool playMoreThanOnce;
+    [Header("Typing")]
+    public Entity cinematicEntity;
+
+    [Header("References")]
+    public Entity interactPrompt;
+
+    BoxCollider boxCollider;
+    bool canBePlayed = true;
+
+    void OnCreate()
+    {
+        boxCollider = entity.GetComponent<BoxCollider>();
+    }
+
+    void OnUpdate()
+    {
+        if (CinematicUI.Instance.IsCinematicOpen || !canBePlayed)
+        {
+            if (interactPrompt.Active)
+                interactPrompt.SetActive(false);
+            return;
+        }
+
+        if (boxCollider.IsColliding)
+        {
+            if (!interactPrompt.Active)
+            {
+                interactPrompt.SetActive(true);
+            }
+
+            if (Player.Instance.Input.interactKeyPressed && canBePlayed)
+            {
+                Open();
+                if (!playMoreThanOnce)
+                {
+                    canBePlayed = false;
+                }
+            }
+        }
+        else
+        {
+            if (interactPrompt.Active)
+            {
+                interactPrompt.SetActive(false);
+            }
+        }
+
+    }
+
+    private void Open()
+    {
+        CinematicUI.Instance.SetUpCinematic(cinematicEntity);
+        CinematicUI.Instance.StartCinematic();
+    }
+};
