b5798a16e15a160e66752e52970ab66f6e78baad
XeivUPC <adria.garcia.almaden@estudiantat.upc.edu>
2026-06-09 02:51:34 +0200

    Animations HealthBar Done

--- Assets/Scripts/UI/HealtBar/HealthHUD.cs
+++ Assets/Scripts/UI/HealtBar/HealthHUD.cs
@@ -1,19 +1,34 @@
 using Loopie;
 using System;
+using System.Collections;
 
 public class HealthHUD : Component
 {
     public int maxHealthIcons = 10;
-    public Entity healthIconsParent;
 
     private HealthSlot[] healthIcons;
     private int lastKnownHealth = -1;
     private int lastKnownMaxHealth = -1;
 
+    [Header("Healt Icons")]
+    public Entity healthIconsParent;
+
+    [Header("Health Bars")]
     public Entity bg4;
     public Entity bg6;
     public Entity bg8;
     public Entity bg10;
+
+    [Header("Health Unlock Anims")]
+    public Entity unlock6;
+    SpriteAnimator unlock6Anim;
+    public float timeUntilUnlock6 = 0.5f;
+    public Entity unlock8;
+    SpriteAnimator unlock8Anim;
+    public float timeUntilUnlock8 = 0.5f;
+    public Entity unlock10;
+    SpriteAnimator unlock10Anim;
+    public float timeUntilUnlock10 = 0.5f;
 
     void OnCreate()
     {
@@ -34,6 +49,11 @@ public class HealthHUD : Component
                 }
             }
         }
+
+        unlock6Anim = unlock6.GetComponent<SpriteAnimator>();
+        unlock8Anim = unlock8.GetComponent<SpriteAnimator>();
+        unlock10Anim = unlock10.GetComponent<SpriteAnimator>();
+
     }
 
     void OnUpdate()
@@ -45,7 +65,12 @@ public class HealthHUD : Component
 
         if (currentHealth != lastKnownHealth || maxHealth != lastKnownMaxHealth)
         {
+            if(maxHealth != lastKnownMaxHealth)
+            {
+                UpdateHUD(maxHealth);
+            }
             UpdateIcons(currentHealth, maxHealth);
+
             lastKnownHealth = currentHealth;
             lastKnownMaxHealth = maxHealth;
         }
@@ -53,11 +78,6 @@ public class HealthHUD : Component
 
     private void UpdateIcons(int currentHealth, int maxHealth)
     {
-        // Toggle the background layouts based on maxHealth
-        if (bg4 != null) bg4.SetActive(maxHealth <= 8);
-        if (bg6 != null) bg6.SetActive(maxHealth > 8 && maxHealth <= 12);
-        if (bg8 != null) bg8.SetActive(maxHealth > 12 && maxHealth <= 16);
-        if (bg10 != null) bg10.SetActive(maxHealth > 16);
 
         int numActiveSlots = (maxHealth + 1) / 2;
 
@@ -72,7 +92,6 @@ public class HealthHUD : Component
             if (i < numActiveSlots)
             {
                 icon.entity.SetActive(true);
-                icon.Unlock();
 
                 if (currentHealth >= 2)
                 {
@@ -95,5 +114,70 @@ public class HealthHUD : Component
                 icon.Lock();
             }
         }
+    }
+
+    public void UpdateHUD(int maxHealth)
+    {
+        bg4.SetActive(false);
+
+
+
+        if (maxHealth == 8)
+        {
+            bg4.SetActive(true);
+            bg6.SetActive(false);
+            bg8.SetActive(false);
+            bg10.SetActive(false);
+        }
+        else if (maxHealth == 12)
+        {
+            StartCoroutine(AnimateUnlocking(bg4, bg6, unlock6Anim, timeUntilUnlock6));
+            bg4.SetActive(true);
+            bg6.SetActive(false);
+            bg8.SetActive(false);
+            bg10.SetActive(false);
+        }
+        else if (maxHealth == 16)
+        {
+            StartCoroutine(AnimateUnlocking(bg6, bg8, unlock8Anim, timeUntilUnlock8));
+
+            bg4.SetActive(false);
+            bg6.SetActive(true);
+            bg8.SetActive(false);
+            bg10.SetActive(false);
+        }
+        else if (maxHealth == 20)
+        {
+            StartCoroutine(AnimateUnlocking(bg8, bg10, unlock10Anim, timeUntilUnlock10));
+
+            bg4.SetActive(false);
+            bg6.SetActive(false);
+            bg8.SetActive(true);
+            bg10.SetActive(false);
+        }
+    }
+
+
+    IEnumerator AnimateUnlocking(Entity oldHUD, Entity newHUD, SpriteAnimator animation, float timeToChange) 
+    {
+        oldHUD.SetActive(true);
+        newHUD.SetActive(false);
+
+        Debug.Log("dsadasdasd");
+        animation.entity.SetActive(true);
+        float timer = 0f;
+        animation.Play();
+        while (timer<timeToChange)
+        {
+            timer+= Time.deltaTime;
+            yield return null;
+        }
+
+        oldHUD.SetActive(false);
+        newHUD.SetActive(true);
+
+        yield return new WaitForSeconds(3);
+
+        animation.entity.SetActive(false);
     }
 };
--- Assets/Scripts/UI/HealtBar/HealthSlot.cs
+++ Assets/Scripts/UI/HealtBar/HealthSlot.cs
@@ -9,22 +9,10 @@ class HealthSlot : Component
     public Image fullSlotImage;
     public Image halfSlotImage;
 
-    private Vector4 originalFullTint = Vector4.One;
-    private Vector4 originalHalfTint = Vector4.One;
-
     void OnCreate()
     {
         fullSlotImage = fullSlotEntity.GetComponent<Image>();
-        if (fullSlotImage != null)
-        {
-            originalFullTint = fullSlotImage.GetTint();
-        }
-
         halfSlotImage = halfSlotEntity.GetComponent<Image>();
-        if (halfSlotImage != null)
-        {
-            originalHalfTint = halfSlotImage.GetTint();
-        }
     }
 
     public void UpdateVisuals(int value)
@@ -37,21 +25,5 @@ class HealthSlot : Component
     {
         halfSlotEntity.SetActive(false);
         fullSlotEntity.SetActive(true);
-        if (fullSlotImage != null)
-        {
-            fullSlotImage.SetTint(new Vector4(0.08f, 0.08f, 0.08f, 1f));
-        }
-    }
-
-    public void Unlock()
-    {
-        if (fullSlotImage != null)
-        {
-            fullSlotImage.SetTint(originalFullTint);
-        }
-        if (halfSlotImage != null)
-        {
-            halfSlotImage.SetTint(originalHalfTint);
-        }
     }
 }
